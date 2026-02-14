f18462ca2106fe303dc27e81e49b74caa3dc1eec
Neeraj jeena <neeraj2005jeena@gmail.com>
2026-02-14 16:39:58 +0530

    sav1

--- school-app/src/pages/Landing.jsx
+++ school-app/src/pages/Landing.jsx
@@ -2,34 +2,35 @@
 // import About from "../components/about/About";
 // import Contact from "../components/contact/Contact";
 // import Footer from "../components/footer/Footer";
+// import ScrollReveal from "../components/ScrollReveal";
 
 // function Landing() {
 //   return (
 //     <>
-//       {/* HERO SECTION */}
-//       <Home />
+//       <ScrollReveal>
+//         <Home />
+//       </ScrollReveal>
 
-//       {/* ABOUT SECTION */}
-//       <section id="about">
+//       <ScrollReveal>
 //         <About />
-//       </section>
+//       </ScrollReveal>
 
-//       {/* CONTACT SECTION */}
-//       <section id="contact">
+//       <ScrollReveal>
 //         <Contact />
-//       </section>
+//       </ScrollReveal>
 
-//       {/* FOOTER */}
-//       <Footer />
+//       <ScrollReveal>
+//         <Footer />
+//       </ScrollReveal>
 //     </>
 //   );
 // }
 
 // export default Landing;
-import Home from "../components/home/Home";
-import About from "../components/about/About";
-import Contact from "../components/contact/Contact";
-import Footer from "../components/footer/Footer";
+import Home from "../components/Home/Home";
+import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
 import ScrollReveal from "../components/ScrollReveal";
 
 function Landing() {
